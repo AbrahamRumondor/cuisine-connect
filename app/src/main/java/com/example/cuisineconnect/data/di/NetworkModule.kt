e7696ff8deeab721e51111d3c5b6c1b71e7bfc60
abrahamrumondor <intern.abraham@gli.id>
2024-08-15 16:37:58 +0700

    styles bnv and start di

---
+++ app/src/main/java/com/example/cuisineconnect/data/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package com.example.cuisineconnect.data.di
+
+import android.content.Context
+import com.example.alfaresto_customersapp.data.network.networkStatusObserver.ConnectivityObserver
+import com.example.alfaresto_customersapp.data.network.networkStatusObserver.NetworkConnectivityObserver
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+  @Provides
+  @Singleton
+  fun provideConnectivityObserver(@ApplicationContext context: Context): ConnectivityObserver {
+    return NetworkConnectivityObserver(context)
+  }
+}
